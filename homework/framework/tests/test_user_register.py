a898047c92137dc6cdcc2598a7eef2f4cf63dcee
annjulyleon <aj.leonova@gmail.com>
2022-05-14 16:46:35 +0300

    #hw4 ex15 added tests for user registrations

--- homework/framework/tests/test_user_register.py
+++ homework/framework/tests/test_user_register.py
@@ -1,22 +1,86 @@
+import allure
+import pytest
+
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
 
+@allure.epic("Registration cases")
 class TestUserRegister(BaseCase):
+    exclude_field = [
+        "firstName",
+        "lastName",
+        "email",
+        "username",
+        "password"
+    ]
+
+    @allure.description("This test ensure user with existing email already exist")
     def test_create_user_with_existing_email(self):
         email = "vinkotov@example.com"
         data = self.prepare_registration_data(email)
 
-        response = MyRequests.post("/user", data = data)
+        response = MyRequests.post("/user", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", \
             f'Unexpected response content {response.content}'
 
+    @allure.description("This test creates user successfully")
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
         response = MyRequests.post("/user", data=data)
 
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
+
+    #  python -m pytest -s .\tests\test_user_register.py -k test_create_user_email_without_at
+    @allure.description("This test ensure user can't be create with email without @")
+    def test_create_user_email_without_at(self):
+        email = "vinkotovexample.com"
+        data = self.prepare_registration_data(email)
+
+        response = MyRequests.post("/user", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"Invalid email format", \
+            f'Unexpected response content {response.content}'
+
+    # python -m pytest -s .\tests\test_user_register.py -k test_create_user_fields
+    @allure.description("This test ensure user can't be created without one of the required field")
+    @pytest.mark.parametrize('exclude_field', exclude_field)
+    def test_create_user_fields(self, exclude_field):
+        data = self.prepare_registration_data()
+        data.pop(exclude_field, None)
+
+        response = MyRequests.post("/user", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.text == f"The following required params are missed: {exclude_field}", \
+            f'Unexpected response text {response.text}'
+
+    # python -m pytest -s .\tests\test_user_register.py -k test_create_user_with_short_field
+    @allure.description("This test ensure user can't be create with too short username or firstname")
+    @pytest.mark.parametrize('field', ["username", "firstName"])
+    def test_create_user_with_short_field(self, field):
+        data = self.prepare_registration_data()
+        data[field] = 'a'
+
+        response = MyRequests.post("/user", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.text == f"The value of '{field}' field is too short", \
+            f'Unexpected response text {response.text}'
+
+    # python -m pytest -s .\tests\test_user_register.py -k test_create_user_with_long_field
+    @allure.description("This test ensure user can't be create with too long username or firstname")
+    @pytest.mark.parametrize('field', ["username", "firstName"])
+    def test_create_user_with_long_field(self, field):
+        data = self.prepare_registration_data()
+        data[field] = 'lqapi' * 51
+
+        response = MyRequests.post("/user", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.text == f"The value of '{field}' field is too long", \
+            f'Unexpected response text {response.text}'
